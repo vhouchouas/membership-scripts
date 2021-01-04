b52c040ad5f824ec17dc98269d8e9a246f0cab0c
Guillaume Turri <guillaume.turri@gmail.com>
2021-01-04 19:01:15 +0100

    rename parameter: s/until/since/
    because it makes more sense from a semantic POV

--- files/MAWhoNeedToRenew.php
+++ files/MAWhoNeedToRenew.php
@@ -3,23 +3,23 @@ define('ZWP_TOOLS', dirname(__FILE__).'/');
 require_once(ZWP_TOOLS . 'lib/mysql.php');
 require_once(ZWP_TOOLS . 'lib/registrationDateUtil.php');
 
-$until = null;
-if (isset($_GET["until"])){
+$since = null;
+if (isset($_GET["since"])){
   try {
-    $until = new DateTime($_GET["until"]);
+    $since = new DateTime($_GET["since"]);
   } catch(Exception $e){
     // Nothing to do. If we reach this point it means the parameter was badly formatted.
-    // We leave `$until` to `null`, it will be handle afterwards
+    // We leave `$since` to `null`, it will be handle afterwards
   }
 }
-if (is_null($until)){
+if (is_null($since)){
   $registrationDateUtil = new RegistrationDateUtil(new DateTime());
-  $until = $registrationDateUtil->getDateAfterWhichMembershipIsConsideredValid();
+  $since = $registrationDateUtil->getDateAfterWhichMembershipIsConsideredValid();
 }
 
 $debug = true; // This page isn't suppose to write anything in db, so let's pretend we're in debug mode
 $mysqlConnector = new MysqlConnector($debug);
-$simplifiedRegistrationEvents = $mysqlConnector->getOrderedListOfLastRegistrations($until);
+$simplifiedRegistrationEvents = $mysqlConnector->getOrderedListOfLastRegistrations($since);
 
 ?>
 <html>
@@ -29,7 +29,7 @@ $simplifiedRegistrationEvents = $mysqlConnector->getOrderedListOfLastRegistratio
 </head>
 <body>
   <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="get" >
-    Remonter jusqu'à <input name="until" type="date" value="<?php echo $until->format("Y-m-d"); ?>" />
+    Remonter jusqu'à <input name="since" type="date" value="<?php echo $since->format("Y-m-d"); ?>" />
     <input type="submit" value="Rafraichir" />
   </form>
 
