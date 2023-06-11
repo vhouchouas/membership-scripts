3071143ce7fecfc90a44655d2ae99fc92b548d29
Guillaume Turri <guillaume.turri@gmail.com>
2023-06-11 23:52:28 +0200

    Add missing fields to MemberDTO
    and fix some column properties

--- files/lib/doctrine/MemberDTO.php
+++ files/lib/doctrine/MemberDTO.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name: 'members')]
 class MemberDTO {
 	#[ORM\Id]
-	#[ORM\Column(type: 'integer')]
+	#[ORM\Column]
 	#[ORM\GeneratedValue]
 	private int|null $id = null;
 
@@ -14,47 +14,41 @@ class MemberDTO {
 		return $this->id;
 	}
 
-	#[ORM\Column(type: 'string')]
-	private string $firstName;
-	public function setFirstName(string $firstName) {
-		$this->firstName = $firstName;
-	}
-	public function getFirstName() : string {
-		return $this->firstName;
-	}
+	#[ORM\Column]
+	public string $firstName;
 
-	#[ORM\Column(type: 'string')]
-	private string $lastName;
-	public function setlastName(string $lstName) {
-		$this->lastName = $lastName;
-	}
-	public function getlastName() : string {
-		return $this->lastName;
-	}
+	#[ORM\Column]
+	public string $lastName;
 
-	#[ORM\Column(type: 'string')]
-	private string $email;
-	public function setemail(string $email) {
-		$this->email = $email;
-	}
-	public function getEmail() : string {
-		return $this->email;
-	}
+	#[ORM\Column(unique: true)]
+	public string $email;
 
-	#[ORM\Column(type: 'string')]
-	private string $postalCode;
-	public function setpostalCode(string $postalCode) {
-		$this->postalCode = $postalCode;
-	}
-	public function getPostalCode() : string {
-		return $this->postalCode;
-	}
+	// Nothing enforces a correct format for postal code on the registration and we sometimes have unexpected strings
+	// eg: P75016. So we give a length a bit bigger than the 5 which should be enough, just in case
+	#[ORM\Column(nullable: true, length: 10)]
+	public string $postalCode;
 
-	// TODO: helloAssoLastRegistreationEventId
-	// TODO: firstRegistrtionDate
-	// TODO: lastRegistrationDate
-	// TODO: city
-	// TODO: isZWProfessional
-	// TODO: howDidYouKnowZwp
-	// TODO: wantToDo
+	#[ORM\Column(unique: true)]
+	public int $helloAssoLastRegistreationEventId;
+
+	#[ORM\Column(nullable: true)]
+	public string $city;
+
+	#[ORM\Column(length: 1000, nullable: true)]
+	public string $howDidYouKnowZwp;
+
+	#[ORM\Column(length: 1000, nullable: true)]
+	public string $wantToDo;
+
+	#[ORM\Column]
+	public DateTime $firstRegistrationDate;
+
+	#[ORM\Column]
+	public DateTime $lastRegistrationDate;
+
+	#[ORM\Column]
+	public bool $isZWProfessional;
+
+	#[ORM\Column]
+	public bool $notificationSentToAdmin = false;
 }
