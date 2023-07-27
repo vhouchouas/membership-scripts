46110036a694baa9120a29461d63ddce4f8d88b1
Guillaume Turri <g.turri@criteo.com>
2023-07-28 00:22:53 +0200

    added more db tests

--- symfony-server/tests/Repository/MemberRepositoryTest.php
+++ symfony-server/tests/Repository/MemberRepositoryTest.php
@@ -50,6 +50,59 @@ final class MemberRepositoryTest extends KernelTestCase {
 		$this->assertEquals(1, count($members), "There should be a single registration after the 'since' date passed");
 	}
 
+	public function test_noUpdateIsPerformedIfTheRegistrationHandledLastIsOlderThanTheCurrentData() {
+		// Setup
+		$debug = false;
+		$bobRegistrationDate = "1985-04-03";
+		$registrationBob = $this->buildHelloassoEvent($bobRegistrationDate, "bob", "dylan", "bob@dylan.com");
+		$this->memberRepository->addOrUpdateMember($registrationBob, $debug);
+
+		// // Precondition check
+		$members = $this->memberRepository->getOrderedListOfLastRegistrations(new DateTime("1800-01-01"));
+		$this->assertExpectedMember($bobRegistrationDate, $bobRegistrationDate, "bob", "dylan", "bob@dylan.com", $members[0]);
+
+		// Act 1: add a previous registration
+		$bobPreviousRegistrationDate = "1985-01-01";
+		$otherRegistrationBob = $this->buildHelloassoEvent($bobPreviousRegistrationDate, "bob", "dylan", "bob@dylan.com");
+		$this->memberRepository->addOrUpdateMember($otherRegistrationBob, $debug);
+
+		// Assert 1
+		$members = $this->memberRepository->getOrderedListOfLastRegistrations(new DateTime("1800-01-01"));
+		$this->assertExpectedMember($bobPreviousRegistrationDate, $bobRegistrationDate, "bob", "dylan", "bob@dylan.com", $members[0]);
+
+		// Act 2: add a registration in between
+		$bobInBetweenRegistrationDate = "1985-02-01";
+		$inBetweenRegistrationBob = $this->buildHelloassoEvent($bobInBetweenRegistrationDate, "bob", "dylan", "bob@dylan.com");
+		$this->memberRepository->addOrUpdateMember($inBetweenRegistrationBob, $debug);
+
+		// Assert 2
+		$members = $this->memberRepository->getOrderedListOfLastRegistrations(new DateTime("1800-01-01"));
+		$this->assertExpectedMember($bobPreviousRegistrationDate, $bobRegistrationDate, "bob", "dylan", "bob@dylan.com", $members[0]);
+	}
+
+	public function test_getListOfRegistrationsOlderThan() {
+		// Setup
+		$debug = false;
+		$bobRegistrationDate = "1985-04-03";
+		$registrationBob = $this->buildHelloassoEvent($bobRegistrationDate, "bob", "dylan", "bob@dylan.com");
+		$aliceRegistrationDate = "1865-11-01";
+		$registrationAlice = $this->buildHelloassoEvent($aliceRegistrationDate, "alice", "wonderland", "al@ice.com");
+
+		// Act
+		$this->memberRepository->addOrUpdateMember($registrationBob, $debug);
+		$this->memberRepository->addOrUpdateMember($registrationAlice, $debug);
+
+		// Assert
+		$this->assertEquals(1, count($this->memberRepository->getListOfRegistrationsOlderThan(new DateTime("1900-01-01"))), "only 1 member registered before that date");
+		$this->assertEquals(2, count($this->memberRepository->getListOfRegistrationsOlderThan(new DateTime("2025-01-01"))), "all 2 members registered before that date");
+
+		// Act & assert again to make sure the last registration date is taken into account
+		$bobUpdateDate = "2030-09-08";
+		$updateBob = $this->buildHelloassoEvent($bobUpdateDate, "bob", "dylan", "bob@dylan.com");
+		$this->memberRepository->addOrUpdateMember($updateBob, $debug);
+		$this->assertEquals(1, count($this->memberRepository->getListOfRegistrationsOlderThan(new DateTime("2025-01-01"))), "The last registration of Bob is now after that date so we should have only alice registration");
+	}
+
 	private $lastHelloAssoEventId = 0;
 	private function buildHelloassoEvent($event_date, $first_name, $last_name, $email): RegistrationEvent {
 		$ret = new RegistrationEvent();
