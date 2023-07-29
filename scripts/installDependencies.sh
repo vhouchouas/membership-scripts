6948a0b216e5264afb76d378cd25cd64dbb3dfc6
Guillaume Turri <g.turri@criteo.com>
2023-07-29 16:36:21 +0200

    Adapt circleCI conf

---
+++ scripts/installDependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Find out directory of current script
+# to make it possible to run this script from any location
+if [ -L "$0" ] && [ -x $(which readlink) ]; then
+  THIS_FILE="$(readlink -mn "$0")"
+else
+  THIS_FILE="$0"
+fi
+SCRIPT_DIR="$(dirname "$THIS_FILE")"
+FILES_DIR="$SCRIPT_DIR/../symfony-server"
+
+"$SCRIPT_DIR"/generate.sh
+
+pushd "$FILES_DIR"
+composer install
+popd
