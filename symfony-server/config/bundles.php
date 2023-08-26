f413d98551ac96ecbeab69ab96578293cae62840
Guillaume Turri <g.turri@criteo.com>
2023-08-26 17:43:30 +0200

    Add symfony/security/bundle

--- symfony-server/config/bundles.php
+++ symfony-server/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
