b08addb0b56186454a23ff57f581a6ac897eb6f2
Guillaume Turri <guillaume.turri@gmail.com>
2023-08-29 13:40:19 +0200

    Add json authentication
    to authenticate:
    
        curl -v -H 'Content-Type: application/json' -d '{"username": "...", "password": "..."}' http://localhost:8000/login
        PHPSESSID=... # read from response header
        curl --cookie "PHPSESSID=$PHPSESSID" http://localhost:8000/api/members-per-postal-code

---
+++ symfony-server/src/Controller/ApiLoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+	#[Route('/login', name: 'api_login')]
+	public function index(#[CurrentUser] ?User $user): JsonResponse
+	{
+		if (null === $user) {
+			return $this->json(['message' => 'missing credentials'], Response::HTTP_UNAUTHORIZED);
+		}
+		return $this->json([
+			'user' => $user->getUserIdentifier(),
+		]);
+	}
+}
