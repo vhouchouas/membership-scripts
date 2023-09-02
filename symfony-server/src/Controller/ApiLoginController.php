a994c22fd539b93c648a2668965df40066243ed6
Guillaume Turri <guillaume.turri@gmail.com>
2023-09-02 21:41:32 +0200

    Add openapi-login.yaml
    It would be more convenient to be able to use generated
    code to perform authentication queries from the front.
    But OTHO we don't want to generate symfony code for those
    endpoints since authentication is handled by Symfony itself.
    So I add those endpoints in a separated openapi file.
    
    Also use a more meaningful field in the response

--- symfony-server/src/Controller/ApiLoginController.php
+++ symfony-server/src/Controller/ApiLoginController.php
@@ -18,7 +18,7 @@ class ApiLoginController extends AbstractController
 			return $this->json(['message' => 'missing credentials'], Response::HTTP_UNAUTHORIZED);
 		}
 		return $this->json([
-			'user' => $user->getUserIdentifier(),
+			'login' => $user->getUserIdentifier(),
 		]);
 	}
 }
