81d0deeff108a9a74d0a6774f56534f36a683652
Guillaume Turri <guillaume.turri@gmail.com>
2023-09-10 22:16:46 +0200

    Create test user in tests setup
    so it's convenient to use in tests that will require one.

--- scripts/runSymfonyTests.sh
+++ scripts/runSymfonyTests.sh
@@ -12,13 +12,16 @@ SCRIPT_DIR="$(dirname "$THIS_FILE")"
 
 cd "$SCRIPT_DIR"/../symfony-server/
 
+export APP_ENV=test
+
 echo "Creating the test database"
-APP_ENV=test php bin/console --env=test doctrine:database:create
+php bin/console --env=test doctrine:database:create
 echo "Dropping the schema of the test database"
-APP_ENV=test php bin/console --env=test doctrine:schema:drop --force
+php bin/console --env=test doctrine:schema:drop --force
 echo "Creating the schema of the test database"
-APP_ENV=test php bin/console --env=test doctrine:schema:create
+php bin/console --env=test doctrine:schema:create
 echo "Initialize some values in the test database"
-APP_ENV=test php bin/console doctrine:database:initialize-last-successful-run-date 2022-01-01
+php bin/console doctrine:database:initialize-last-successful-run-date 2022-01-01
+php bin/console user:add testuser testpassword
 
 APP_ENV=test XDEBUG_MODE=coverage php bin/phpunit --coverage-html coverage $@
--- symfony-server/tests/Command/UserCommandsTest.php
+++ symfony-server/tests/Command/UserCommandsTest.php
@@ -9,39 +9,40 @@ class UserCommandsTest extends KernelTestCase
 	public function testExecute(): void {
 		$kernel = self::bootKernel();
 		$application = new Application($kernel);
+		$userEmail = "some@name.fr";
 
 		// Precondition: no user should exist
 		$listUsersCommand = new CommandTester($application->find('user:list'));
 		$listUsersCommand->execute([]);
 		$listUsersCommand->assertCommandIsSuccessful();
 		$output = $listUsersCommand->getDisplay();
-		$this->assertEmpty($output, "Precondition: we expect no user at that poin");
+		$this->assertStringNotContainsString($userEmail, $output, "Precondition: the user that this test will create, should not exist yet");
 
 		// Create a user
 		$addUserCommand = new CommandTester($application->find('user:add'));
-		$addUserCommand->execute(["email" => "some@name.fr", "password" => "mypassword"]);
+		$addUserCommand->execute(["email" => $userEmail, "password" => "mypassword"]);
 		$addUserCommand->assertCommandIsSuccessful();
 
 		// // Assert user is created
 		$listUsersCommand->execute([]);
 		$listUsersCommand->assertCommandIsSuccessful();
 		$output = $listUsersCommand->getDisplay();
-		$this->assertStringContainsString("some@name.fr", $output, "user should have been created");
+		$this->assertStringContainsString($userEmail, $output, "user should have been created");
 
 		// Test password update command (just ensure it does not crash, no assertions here)
 		$updateUserPasswordCommand = new CommandTester($application->find('user:update-password'));
-		$updateUserPasswordCommand->execute(["email" => "some@name.fr", "password" => "my-new-password"]);
+		$updateUserPasswordCommand->execute(["email" => $userEmail, "password" => "my-new-password"]);
 		$updateUserPasswordCommand->assertCommandIsSuccessful();
 
 		// Delete user
 		$deleteUserCommand = new CommandTester($application->find('user:delete'));
-		$deleteUserCommand->execute(["email" => "some@name.fr"]);
+		$deleteUserCommand->execute(["email" => $userEmail]);
 		$deleteUserCommand->assertCommandIsSuccessful();
 
 		// // Assert user is deleted
 		$listUsersCommand->execute([]);
 		$listUsersCommand->assertCommandIsSuccessful();
 		$output = $listUsersCommand->getDisplay();
-		$this->assertEmpty($output, "No users should be left");
+		$this->assertStringNotContainsString($userEmail, $output, "The user created by this test should have been deleted");
 	}
 }
